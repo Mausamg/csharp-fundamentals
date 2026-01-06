877b00e608856b0b93cb8b0d3ea0cbe5285c354c
Mausamg <mausamshrestha1200@gmail.com>
2026-01-06 20:53:00 +0545

    add the concept of switch case statement

--- ControlStatement/Program.cs
+++ ControlStatement/Program.cs
@@ -35,11 +35,42 @@ namespace ControlStatement
 
             if (answer == multiplication)
             {
-                Console.WriteLine("Correct answer!");
+                Console.WriteLine("Your answer is Correct!");
             }
             else
             {
                 Console.WriteLine("Wrong answer. The correct answer is " + multiplication);
+            }
+
+            Console.WriteLine("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
             }
             Console.ReadLine();
         }
