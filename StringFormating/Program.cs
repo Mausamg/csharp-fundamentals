898eadb20f60dc6c70b5e4e39fe1ee2325ea1b2e
Mausamg <mausamshrestha1200@gmail.com>
2026-01-09 19:23:44 +0545

    add the string formatting project

---
+++ StringFormating/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string speech = "He said, \"Hello, World!\"";
+            Console.WriteLine(speech);
+             string path = @"C:\Program Files\\MyApp\app.exe"+ "\nNew line here ";
+            Console.WriteLine(path);
+        }
+    }
+}
