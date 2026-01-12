767fd75559edeaf4685879d06efcbfd24253973e
Mausamg <mausamshrestha1200@gmail.com>
2026-01-12 18:29:10 +0545

    Implement arrays, lists, and dictionaries in C#

--- arrays-lists-dictionaries/Program.cs
+++ arrays-lists-dictionaries/Program.cs
@@ -10,84 +10,84 @@ namespace arrays_lists_dictionaries
     {
         static void Main(string[] args)
         {
-            //Arrays in C#
-            // Console.WriteLine("Enter the size of Array : ");
-            // int i =Convert.ToInt32(Console.ReadLine());
-            // int[] numbers= new int[i];
+            Arrays in C#
+             Console.WriteLine("Enter the size of Array : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[i];
 
-            // for(int j=0; j < i; j++)
-            // {
-            //     Console.WriteLine("Enter the element {0} : ",j+1);
-            //     numbers[j]=Convert.ToInt32(Console.ReadLine());
-            // }
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine("Enter the element {0} : ", j + 1);
+                numbers[j] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            // Console.Write("The elements in the array are :");
-            // for(int j=0; j < i; j++)
-            // {
-            //     Console.Write($" {numbers[j]}");
-            // }
-            //Console.WriteLine("\n The array elements are :");
-            // foreach (int j in numbers)
-            //     {
-            //     Console.Write($"{j}");
-            // }
+            Console.Write("The elements in the array are :");
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write($" {numbers[j]}");
+            }
+            Console.WriteLine("\n The array elements are :");
+            foreach (int j in numbers)
+            {
+                Console.Write($"{j}");
+            }
 
-            //int sum = 0;
-            //int[] angles = new int[3];
-            //for (int i = 0; i < angles.Length; i++)
-            //{
-            //    Console.WriteLine($"Enter the  angle {i + 1}:");
-            //    angles[i] = Convert.ToInt32(Console.ReadLine());
-            //    sum += angles[i];
-            //}
-            //Console.WriteLine("The sum of angles is : " + sum);
-            //if (angles.Length != 0)
-            //{
-            //    if (sum == 180)
-            //    {
-            //        Console.WriteLine("The angles  form a triangle");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Invalid, The total sum of angles must be 180.");
+            int sum = 0;
+            int[] angles = new int[3];
+            for (int i = 0; i < angles.Length; i++)
+            {
+                Console.WriteLine($"Enter the  angle {i + 1}:");
+                angles[i] = Convert.ToInt32(Console.ReadLine());
+                sum += angles[i];
+            }
+            Console.WriteLine("The sum of angles is : " + sum);
+            if (angles.Length != 0)
+            {
+                if (sum == 180)
+                {
+                    Console.WriteLine("The angles  form a triangle");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid, The total sum of angles must be 180.");
 
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine("The angles doesnot form a triangle");
-            //}
+                }
+            }
+            else
+            {
+                Console.WriteLine("The angles doesnot form a triangle");
+            }
 
-            ////Array Sorting
-            //Array.Sort(angles);
-            //Console.WriteLine("The sorted angles are : ");
-            //foreach (int i in angles)
-            //{
-            //    Console.Write($"{i} ");
+            //Array Sorting
+            Array.Sort(angles);
+            Console.WriteLine("The sorted angles are : ");
+            foreach (int i in angles)
+            {
+                Console.Write($"{i} ");
 
-            //}
+            }
 
 
-            ////Array Reverse
-            ////Array.Reverse(angles);
-            //Console.Write("\n The reversed angles are : ");
-            //foreach (int i in angles)
-            //{
-            //    Console.Write($"{i} ");
-            //}
+            //Array Reverse
+            //Array.Reverse(angles);
+            Console.Write("\n The reversed angles are : ");
+            foreach (int i in angles)
+            {
+                Console.Write($"{i} ");
+            }
 
-            //Console.WriteLine("\nEnter a number to search :");
-            //int search = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter a number to search :");
+            int search = Convert.ToInt32(Console.ReadLine());
 
-            //int position= Array.IndexOf(angles, search,1,2);
-            //if (position > -1)
-            //{
-            //    Console.WriteLine($"The {search} number has been found in postion{position + 1}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"{search} not found");
-            //}
+            int position = Array.IndexOf(angles, search, 1, 2);
+            if (position > -1)
+            {
+                Console.WriteLine($"The {search} number has been found in postion{position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"{search} not found");
+            }
 
 
             //Lists in C#
@@ -108,6 +108,46 @@ namespace arrays_lists_dictionaries
             for (int i = 0; i < num1.Count; i++)
             {
                 Console.Write(num1[i]);
+            }
+            Console.WriteLine();
+            //Dictionary in C#
+            Dictionary<int, string> names = new Dictionary<int, string>
+            {
+                //KeyValue pairs
+                {1, "Alice"  },
+                {2, "Bob" },
+                {3, "Charlie"
+            }
+            };
+            //names.Add(1, "Alice");
+            //names.Add(2, "Bob");
+            //names.Add(3, "Charlie");
+
+            for(int i=1; i <= names.Count; i++)
+            {
+                //Console.WriteLine("Key: {0}, Value: {1}", i, names[i]);
+                KeyValuePair<int, string> pair = names.ElementAt(i);
+                Console.WriteLine("Key: {0}, Value: {1}", pair.Key, pair.Value);
+            }
+
+            foreach(var pair in names)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", pair.Key, pair.Value);
+            }
+
+            Dictionary<string,string> teachers = new Dictionary<string, string>
+            {     {"Maths", "Mr. Smith" },
+                {"Science", "Ms. Johnson" },
+                {"History", "Mr. Brown" }
+            };
+
+            if (teachers.TryGetValue("Science", out string teacherName))
+            {
+                Console.WriteLine("The teacher for Science is: " + teacherName);
+            }
+            else
+            {
+                Console.WriteLine("Subject not found.");
             }
         }
     }
