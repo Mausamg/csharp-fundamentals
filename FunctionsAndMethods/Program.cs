defd8aae31783d63c01c2a95d6e5ed478a66fb3d
Mausamg <mausamshrestha1200@gmail.com>
2026-01-13 15:26:50 +0545

    add the Implement basic methods, TryParse, and array handling

--- FunctionsAndMethods/Program.cs
+++ FunctionsAndMethods/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FunctionsAndMethods
 {
-    internal class Program
+    class Test
     {
-        static void Main(string[] args)
+        public  static void SampleMethod()
         {
+            Console.WriteLine("This is a sample method in Test class.");
         }
     }
-}
+
+
+        internal class Program
+        {
+            static void ExampleMethod()
+            {
+                Console.WriteLine("This is an example method.");
+            }
+
+        static void PrintArray()
+        {
+            Console.WriteLine("Enter the size of array : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] arrayNum = new int[num];
+            
+            for(int i=0;i<num;i++)
+            {
+                Console.WriteLine($"Enter the {i + 1} array elememt :");
+                arrayNum[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            int counter = 0;
+            Console.WriteLine("Using foreach loop: ");
+            foreach (int n in arrayNum)
+            {
+                Console.WriteLine($"The array element {counter+1} is : {n}");
+                counter++;
+            }
+
+
+            Console.WriteLine("Using for loop: ");
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"The array element {i + 1} is : {arrayNum[i]}");
+            }
+           
+        }
+
+            static void Main(string[] args)
+            {
+                ExampleMethod();
+                
+                Test.SampleMethod();
+                
+            bool success = int.TryParse("123", out int result);
+            Console.WriteLine(result);
+            Console.WriteLine(success);
+
+            PrintArray();
+                Console.ReadLine();
+            }
+        }
+    }
+
