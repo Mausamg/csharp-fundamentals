6cd119ab314f00dc16bc32418471d491c8ba496d
Mausamg <mausamshrestha1200@gmail.com>
2026-01-16 18:54:24 +0545

    add the function for area of triangle

--- Function-exercise/Program.cs
+++ Function-exercise/Program.cs
@@ -8,8 +8,26 @@ namespace Function_exercise
 {
     internal class Program
     {
+        static void AreaTriangle(int len, int bre)
+        {
+
+            double AreaTriangele = 0.5 * (len * bre);
+            Console.WriteLine("The Area of Triangle is "+AreaTriangele);
+        }
+
+        static void ReadTriangle()
+        {
+            Console.WriteLine("Enter the length of the Triangle :");
+            int len=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Height of the Triangle :");
+            int bre=Convert.ToInt32(Console.ReadLine());
+
+            AreaTriangle(len, bre);
+        }
         static void Main(string[] args)
         {
+            ReadTriangle();
         }
     }
 }
